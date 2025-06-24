fb62cda46789c758d82e41aa0148db7bb9f89803
Yuji Miranda Kunitake <andrekuni123@gmail.com>
2025-06-24 12:58:30 -0300

    feat: include get_current_user function.

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -1,14 +1,18 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from jose import jwt, JWTError
 from typing import Optional
 from dotenv import load_dotenv
 import os
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -20,5 +24,43 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def decode_token(token: str):
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+def decode_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token signature os structure invalid.",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+def get_current_user(token: str = Depends(oauth2_scheme)) -> int:
+    try:
+        payload = decode_token(token)
+
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token payload missing 'sub' field.",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        
+
+        return int(user_id)
+    except (ValueError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="'sub' field in token in not a valid integer.",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    
+    except HTTPException:
+        raise
+
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Unexpected error while decoding token."
+        )
